87a68887f1ba7e83465377c8a311f3ccda36e46b
Muhammad Abdullah <97453802+TheAbdullahChaudhary@users.noreply.github.com>
2024-07-28 04:18:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'Well done! You successfully deployed your Flask app, Now you can start building your app'
+        return 'Well done! You successfully deployed your Flask app, Now you can start building your app - updated'
 
     return app
 if __name__ == '__main__':
